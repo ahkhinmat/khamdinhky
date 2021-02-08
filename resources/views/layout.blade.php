fdd32de7c43eeb6685233ed045b72956530aa910
ta kha <ta.minhkha@gmail.com>
2021-02-08 09:07:48 +0700

    ssl

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -9,12 +9,12 @@ $result_thongtin=Session::get('thongtin_chung');
 	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
 	<title>Cổng bệnh án khám định kỳ Bệnh viện Hoàn Mỹ Đà Nẵng</title>
 	<meta content='width=device-width, initial-scale=1.0, shrink-to-fit=no' name='viewport' />
-	{{-- <link href="{{asset('frontend/css/prettyPhoto.css')}}" rel="stylesheet"> --}}
-{{-- <link rel="icon" href="{{asset('assets/img/favicon.ico')}}" type="image/x-icon"/> --}}
-<link rel="icon" href="{{asset('assets/img/logo-hmdn.png')}}" sizes="20x20" type="image/png">
+	{{-- <link href="{{secure_asset('frontend/css/prettyPhoto.css')}}" rel="stylesheet"> --}}
+{{-- <link rel="icon" href="{{secure_asset('assets/img/favicon.ico')}}" type="image/x-icon"/> --}}
+<link rel="icon" href="{{secure_asset('assets/img/logo-hmdn.png')}}" sizes="20x20" type="image/png">
 
 	<!-- Fonts and icons -->
-	<script src="{{asset('assets/js/plugin/webfont/webfont.min.js')}}"></script>
+	<script src="{{secure_asset('assets/js/plugin/webfont/webfont.min.js')}}"></script>
 	<script>
 		WebFont.load({
 			google: {"families":["Open+Sans:300,400,600,700"]},
@@ -26,12 +26,12 @@ $result_thongtin=Session::get('thongtin_chung');
 	</script>
 
 	<!-- CSS Files -->
-	<link rel="stylesheet" href="{{asset('assets/css/style.css')}}">
-	<link rel="stylesheet" href="{{asset('assets/css/bootstrap.min.css')}}">
-	<link rel="stylesheet" href="{{asset('assets/css/azzara.min.css')}}">
+	<link rel="stylesheet" href="{{secure_asset('assets/css/style.css')}}">
+	<link rel="stylesheet" href="{{secure_asset('assets/css/bootstrap.min.css')}}">
+	<link rel="stylesheet" href="{{secure_asset('assets/css/azzara.min.css')}}">
 
 	<!-- CSS Just for demo purpose, don't include it in your project -->
-	<link rel="stylesheet" href="{{asset('assets/css/demo.css')}}">
+	<link rel="stylesheet" href="{{secure_asset('assets/css/demo.css')}}">
 </head>
 <body>
 	<div class="wrapper">
@@ -42,7 +42,7 @@ $result_thongtin=Session::get('thongtin_chung');
 			<!-- Logo Header -->
 			<div class="logo-header">
 				<a href="dashboard" class="logo">
-				<img src="{{asset('assets/img/logohmdn.png')}}" alt="..." class="avatar-img rounded-circle logo">
+				<img src="{{secure_asset('assets/img/logohmdn.png')}}" alt="..." class="avatar-img rounded-circle logo">
 				</a>
 				<button class="navbar-toggler sidenav-toggler ml-auto" type="button" data-toggle="collapse" data-target="collapse" aria-expanded="false" aria-label="Toggle navigation">
 					<span class="navbar-toggler-icon">
@@ -294,53 +294,53 @@ $result_thongtin=Session::get('thongtin_chung');
 	</div>
 </div>
 <!--   Core JS Files   -->
-<script src="{{asset('assets/js/core/jquery.3.2.1.min.js')}}"></script>
-<script src="{{asset('assets/js/core/popper.min.js')}}"></script>
-<script src="{{asset('assets/js/core/bootstrap.min.js')}}"></script>
+<script src="{{secure_asset('assets/js/core/jquery.3.2.1.min.js')}}"></script>
+<script src="{{secure_asset('assets/js/core/popper.min.js')}}"></script>
+<script src="{{secure_asset('assets/js/core/bootstrap.min.js')}}"></script>
 
 <!-- jQuery UI -->
-<script src="{{asset('assets/js/plugin/jquery-ui-1.12.1.custom/jquery-ui.min.js')}}"></script>
-<script src="{{asset('assets/js/plugin/jquery-ui-touch-punch/jquery.ui.touch-punch.min.js')}}"></script>
+<script src="{{secure_asset('assets/js/plugin/jquery-ui-1.12.1.custom/jquery-ui.min.js')}}"></script>
+<script src="{{secure_asset('assets/js/plugin/jquery-ui-touch-punch/jquery.ui.touch-punch.min.js')}}"></script>
 
 <!-- jQuery Scrollbar -->
-<script src="{{asset('assets/js/plugin/jquery-scrollbar/jquery.scrollbar.min.js')}}"></script>
+<script src="{{secure_asset('assets/js/plugin/jquery-scrollbar/jquery.scrollbar.min.js')}}"></script>
 
 <!-- Moment JS -->
-<script src="{{asset('assets/js/plugin/moment/moment.min.js')}}"></script>
+<script src="{{secure_asset('assets/js/plugin/moment/moment.min.js')}}"></script>
 
 <!-- Chart JS -->
-<script src="{{asset('assets/js/plugin/chart.js/chart.min.js')}}"></script>
+<script src="{{secure_asset('assets/js/plugin/chart.js/chart.min.js')}}"></script>
 
 <!-- jQuery Sparkline -->
-<script src="{{asset('assets/js/plugin/jquery.sparkline/jquery.sparkline.min.js')}}"></script>
+<script src="{{secure_asset('assets/js/plugin/jquery.sparkline/jquery.sparkline.min.js')}}"></script>
 
 <!-- Chart Circle -->
-<script src="{{asset('assets/js/plugin/chart-circle/circles.min.js')}}"></script>
+<script src="{{secure_asset('assets/js/plugin/chart-circle/circles.min.js')}}"></script>
 
 <!-- Datatables -->
-<script src="{{asset('assets/js/plugin/datatables/datatables.min.js')}}"></script>
+<script src="{{secure_asset('assets/js/plugin/datatables/datatables.min.js')}}"></script>
 
 <!-- Bootstrap Notify -->
-<script src="{{asset('assets/js/plugin/bootstrap-notify/bootstrap-notify.min.js')}}"></script>
+<script src="{{secure_asset('assets/js/plugin/bootstrap-notify/bootstrap-notify.min.js')}}"></script>
 
 <!-- Bootstrap Toggle -->
-<script src="{{asset('assets/js/plugin/bootstrap-toggle/bootstrap-toggle.min.js')}}"></script>
+<script src="{{secure_asset('assets/js/plugin/bootstrap-toggle/bootstrap-toggle.min.js')}}"></script>
 
 <!-- jQuery Vector Maps -->
-<script src="{{asset('assets/js/plugin/jqvmap/jquery.vmap.min.js')}}"></script>
-<script src="{{asset('assets/js/plugin/jqvmap/maps/jquery.vmap.world.js')}}"></script>
+<script src="{{secure_asset('assets/js/plugin/jqvmap/jquery.vmap.min.js')}}"></script>
+<script src="{{secure_asset('assets/js/plugin/jqvmap/maps/jquery.vmap.world.js')}}"></script>
 
 <!-- Google Maps Plugin -->
-<script src="{{asset('assets/js/plugin/gmaps/gmaps.js')}}"></script>
+<script src="{{secure_asset('assets/js/plugin/gmaps/gmaps.js')}}"></script>
 
 <!-- Sweet Alert -->
-<script src="{{asset('assets/js/plugin/sweetalert/sweetalert.min.js')}}"></script>
+<script src="{{secure_asset('assets/js/plugin/sweetalert/sweetalert.min.js')}}"></script>
 
 <!-- Azzara JS -->
-<script src="{{asset('assets/js/ready.min.js')}}"></script>
+<script src="{{secure_asset('assets/js/ready.min.js')}}"></script>
 
 <!-- Azzara DEMO methods, don't include it in your project! -->
-<script src="{{asset('assets/js/setting-demo.js')}}"></script>
-<script src="{{asset('assets/js/demo.js')}}"></script>
+<script src="{{secure_asset('assets/js/setting-demo.js')}}"></script>
+<script src="{{secure_asset('assets/js/demo.js')}}"></script>
 </body>
 </html>
